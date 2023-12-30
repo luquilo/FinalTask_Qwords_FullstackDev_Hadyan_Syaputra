d42aac75a59db11dd37908bc7b627d89cd9fa5bd
luquilo <luquilo.etc@gmail.com>
2023-12-30 17:45:45 +0700

    navbar added

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -9,8 +9,29 @@
 
 
     </head>
-    <body>
-        <h1>halo</h1><h2>halo lagi</h2>
-        
+    <body class="flex flex-col justify-center items-center">
+        <section id="navbar" class="flex justify-between p-4">
+            <div><img src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/logo-qw-light.webp" alt="qwords" class="pl-4"></div>
+            <div class="flex pt-2">
+                <p class="p-2 cursor-pointer hover:text-[#ff6d0b] font-bold">Cloud Hosting</p>
+                <p class="p-2 cursor-pointer hover:text-[#ff6d0b] font-bold">Server</p>
+                <p class="p-2 cursor-pointer hover:text-[#ff6d0b] font-bold">Domain</p>
+                <p class="p-2 cursor-pointer hover:text-[#ff6d0b] font-bold">Email Suite</p>
+                <p class="p-2 cursor-pointer hover:text-[#ff6d0b] font-bold">Services</p>
+                <p class="p-2 cursor-pointer hover:text-[#ff6d0b] font-bold">Internet Access</p>
+                <p class="p-2 cursor-pointer hover:text-[#ff6d0b] font-bold">Promo</p>
+            </div>
+            <div class=" font-bold cursor-pointer text-[#ff6d0b] border-2 justify-center items-center p-2 border-[#ff6d0b] rounded-lg hover:text-[#fff] hover:bg-[#ff6d0b]">
+                <p>log in</p>
+            </div>
+        </section>
+
+
+        <section id="banner">
+            <h1 class="text-6xl font-bold">Cloud Hosting untuk Kesusksesan Websitemu</h1>
+            <p>Qwords merupakan perusahaan teknologi yang menyediakan layanan cloud hosting, data center solution, domain, dan layanan digital lainnya</p>
+        </section>
+
+
     </body>
 </html>
